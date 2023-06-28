41c17a0903c7b170ec1499612a50d197d5440802
Faust <verxe@list.ru>
2023-06-28 15:25:16 +0300

    Привязали принтер к коду

--- main.py
+++ main.py
@@ -1,22 +1,49 @@
-import cv2
 import time
+import serial
+import cv2
 
-cap = cv2.VideoCapture(0)  # захват камеры
+def take_photo():
+    cap = cv2.VideoCapture(0)  # захват камеры
+    o = 0
+    while True:  # цикл позволяет снимать видео
 
-while True:  # цикл позволяет снимать видео
+        o += 1
+        ret, img = cap.read()
+        if ret:
+            cv2.imshow("camera", img)
 
-    ret, img = cap.read()
-    if ret:
-        cv2.imshow("camera", img)
+        else:
+            print("error")
 
-    else:
-        print("error")
+        if o == 50:
+            cv2.imwrite("camera.png", img)
+            break
 
-    if cv2.waitKey(10) == 27:
-        cv2.imwrite("camera.png", img)
-        break
+    cap.release()
+    cv2.destroyAllWindows()
+    return
 
-cap.release()
-cv2.destroyAllWindows()
+# Установка соединения через последовательный порт
+ser = serial.Serial('COM3', 115200)
+time.sleep(2)
 
+# примеры комманд
+command = "G28\r\n"
+ser.write(command.encode())
+command = "G0 X100 Y100\r\n"
+ser.write(command.encode())
+command = "G0 X0 Y220\r\n"  # выдвигаем стол для фото
+ser.write(command.encode())
 
+# делаем фото
+take_photo()
+
+# продолжаем команды
+command = "G4 S2\r\n"
+ser.write(command.encode())
+command = "G0 X110 Y110\r\n"
+ser.write(command.encode())
+
+time.sleep(20)
+
+ser.close()
