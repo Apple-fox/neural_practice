e8bf873cc72cfb5c0505653b9838ee7b5841a75e
Apfox <ishemtseva@yandex.ru>
2023-06-28 15:54:53 +0500

    Считываем команды из gcode-файла

--- main.py
+++ main.py
@@ -27,23 +27,36 @@ def take_photo():
 ser = serial.Serial('COM3', 115200)
 time.sleep(2)
 
-# примеры комманд
-command = "G28\r\n"
-ser.write(command.encode())
-command = "G0 X100 Y100\r\n"
-ser.write(command.encode())
-command = "G0 X0 Y220\r\n"  # выдвигаем стол для фото
-ser.write(command.encode())
-
-# делаем фото
-take_photo()
-
-# продолжаем команды
-command = "G4 S2\r\n"
-ser.write(command.encode())
-command = "G0 X110 Y110\r\n"
-ser.write(command.encode())
-
-time.sleep(20)
+# # примеры комманд
+# command = "G28\r\n"
+# ser.write(command.encode())
+# command = "G0 X100 Y100\r\n"
+# ser.write(command.encode())
+# command = "G0 X0 Y220\r\n"  # выдвигаем стол для фото
+# ser.write(command.encode())
+#
+# # делаем фото
+# take_photo()
+#
+# # продолжаем команды
+# command = "G4 S2\r\n"
+# ser.write(command.encode())
+# command = "G0 X110 Y110\r\n"
+# ser.write(command.encode())
+#
+# time.sleep(20)
+f = open("key_chain.gcode", "r")
+a = f.readline()
+while(a):
+    s = a.split(":")
+    if ("LAYER" in s[0] and "LAYER_COUNT" not in s[0] and s[1] != "0\n"):  # если попадается строка означающая смену слоя
+        command = "G0 X0 Y220\r\n"  # отправляем команду на выдвигание стола
+        ser.write(command.encode())
+        take_photo()  # делаем снимок
+    else:
+        command = a
+        ser.write(command.encode())
+    a = f.readline()
+f.close()
 
 ser.close()
